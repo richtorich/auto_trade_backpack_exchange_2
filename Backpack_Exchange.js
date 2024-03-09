06974e8f1b894e185870cafa5750391dccef4293
Reachtorich <149753418+richtorich@users.noreply.github.com>
2024-03-09 22:13:52 +0700

    Update Backpack_Exchange.js

--- Backpack_Exchange.js
+++ Backpack_Exchange.js
@@ -1,8 +1,8 @@
 
 /////// Setting here ///////
 const number = 1000; // total number buy/sell
-const minSleep = 5; // 5s
-const maxSleep = 10; // 10s
+const minSleep = 10; // 10s
+const maxSleep = 30; // 30s
 ///////////////////////////
 
 let count = 0;
